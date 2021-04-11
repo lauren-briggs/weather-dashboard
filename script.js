0aa3fd08c176d2970230e92cfbffb6073d1bb3ea
lauren-briggs <laurenbriggs41@gmail.com>
2021-04-11 21:57:03 +1000

    work on setting up local storage and more fetch requests

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var searchInput = document.getElementById('searchInput');
-var city = searchInput.value;
 var searchBtn = document.getElementById('searchBtn');
 
 var searchHistoryEl = document.getElementById('searchHistory');
@@ -16,11 +15,15 @@ var uvIndexEl = document.getElementById('uvIndex');
 var foreCastEl = document.getElementById('forecast');
 
 var apiKey = "5d5d1586256ab99773fad7bd1dad7135";
+var city = searchInput.value;
+
+var prevSearches = [];
 
 // get current date
 function getDate() {
     //variable holding the current day and date using moment.js
     var currentDay = moment().format("dddd, Do MMMM");
+    dateEl.innerHTML = "";
     dateEl.append(currentDay);
     console.log("Today is " + currentDay);
 }
@@ -32,13 +35,21 @@ function handleSearch(e) {
     if (searchInput.value === "") {
         alert("Please enter a city");
     } else {
+        //save to local storage
+        var city = searchInput.value;
+        prevSearches.unshift(city);
+        window.localStorage.setItem("City", JSON.stringify(prevSearches));
+        console.log("You have saved your search of " + city + " to local storage");
+        console.log("Previous searches array: " + prevSearches)
+
         getAPI();
+        getForecast();
     }
 };
 
 // make API call
 function getAPI() {
-    var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=" + apiKey;
 
     //fetch request
     fetch(requestURL)
@@ -53,17 +64,58 @@ function getAPI() {
                     })
             }
 
+        }).catch(function (error) {
+            console.log(error);
+            return error;
         })
 };
 
 //render weather results to screen
 function renderWeather(weather) {
 
+    // var searchedCity = searchInput.value;
+    cityNameEl.innerHTML = "<h2>" + searchInput.value + "</h2>";
+    cityNameEl.setAttribute("style", "text-transform: uppercase;")
+    console.log("The city you are searching is " + searchInput.value);
+
     getDate();
 
-    var currentDay = moment().format("dddd, Do MMMM");
-    tempEl.append(currentDay);
-    console.log("The temperature is " + currentDay);
+    weatherCondEl.innerHTML = ""
+    var weatherIcon = weatherCondEl.createElement("img");
+    weatherIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + weather.icon[0] + "@2x.png");
+
+    tempEl.append(weather.main.temp);
+    humidityEl.append(weather.main.humidity);
+    windSpeedEl.append(weather.wind.speed);
+    uvIndexEl.append()
 };
 
-searchBtn.addEventListener('click', handleSearch);
+
+//forecast function for API call
+function getForecast() {
+    var requestURL = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}"
+
+    fetch(requestURL)
+        .then(function (response) {
+            response.json().then(function (data) {
+                renderForecast(data);
+                console.log(data);
+            });
+        });
+
+}
+
+
+// funciton renderForecast(){
+
+// }
+
+
+
+searchBtn.addEventListener('click', handleSearch, renderHistory);
+
+function renderHistory() {
+    var prevSearchList = JSON.parse(localStorage.getItem("City"));
+    document.getElementById('searchHistory').innerHTML = prevSearchList;
+
+}
