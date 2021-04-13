39e3350b0f9e6a5a3b88a66836f4423fee8dc8c7
lauren-briggs <laurenbriggs41@gmail.com>
2021-04-13 22:32:26 +1000

    finished 5 day forecast and fixed date bug

--- script.js
+++ script.js
@@ -12,22 +12,12 @@ var humidityEl = document.getElementById('humidity');
 var windSpeedEl = document.getElementById('windSpeed');
 var uvIndexEl = document.getElementById('uvIndex');
 
-var forecastEl = document.getElementById('forecast');
+var forecastEl = document.getElementById('forecast-grid-container');
 
 var apiKey = "5d5d1586256ab99773fad7bd1dad7135";
 var city = searchInput.value;
 
 var prevSearches = JSON.parse(window.localStorage.getItem("City"));
-
-// get current date
-function getDate() {
-    //variable holding the current day and date using moment.js
-    var currentDay = moment().format("dddd, Do MMMM");
-    dateEl.innerHTML = "";
-    dateEl.append(currentDay);
-    console.log("Today is " + currentDay);
-}
-
 
 // search function
 function handleSearch(e) {
@@ -36,12 +26,11 @@ function handleSearch(e) {
         alert("Please enter a city");
     } else {
         //save to local storage
-        city = searchInput.value;
+        city = searchInput.value.toUpperCase();
         if (!prevSearches.includes(city)) {
             prevSearches.unshift(city);
             window.localStorage.setItem("City", JSON.stringify(prevSearches));
-            console.log("You have saved your search of " + city + " to local storage");
-            console.log("Previous searches array: " + prevSearches)
+            console.log(prevSearches)
         }
         getLatLon();
     }
@@ -54,25 +43,34 @@ function renderWeather(data) {
     cityNameEl.setAttribute("style", "text-transform: uppercase;")
     console.log("The city you are searching is " + searchInput.value);
 
-    getDate();
+    //using UNIX code from data to get current date
+    var currentDay = moment.utc().format("dddd, Do MMMM");
+    dateEl.textContent = "";
+    dateEl.append(currentDay);
+    console.log("Today is " + currentDay);
 
-    console.log("api ONE call")
-    console.log(data);
-
+    //getting weather icon
     weatherCondEl.innerHTML = ""
     var weatherIcon = document.createElement("img");
     weatherIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
     weatherCondEl.appendChild(weatherIcon);
 
-    tempEl.innerHTML = data.current.temp;
-    humidityEl.innerHTML = data.current.humidity;
+    //temperature, humidity, wind speed and uv index
+    tempEl.innerHTML = data.current.temp + "&#8451;";
+    humidityEl.innerHTML = data.current.humidity + "%";
     windSpeedEl.innerHTML = data.current.wind_speed;
     uvIndexEl.innerHTML = data.current.uvi;
+
+    //setting background colour of uv index depending on integer
+    if (data.current.uvi > 5) {
+        uvIndexEl.setAttribute("style", "background-color: red;")
+    } else if (data.current.uvi <= 5 || data.current.uvi >= 3) {
+        uvIndexEl.setAttribute("style", "background-color: orange;")
+    } else {
+        uvIndexEl.setAttribute("style", "background-color: yellow;")
+    };
+
 };
-
-
-
-
 
 //forecast function for API call
 function getLatLon() {
@@ -82,7 +80,7 @@ function getLatLon() {
         .then(function (response) {
             response.json().then(function (data) {
                 getForecast(data);
-                console.log("APi call to get Lat and Lon below")
+                console.log("DATA from APi call to get Lat and Lon below")
                 console.log(data);
                 renderForecast(data);
             });
@@ -90,7 +88,7 @@ function getLatLon() {
 }
 
 function getForecast(data) {
-    const requestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}`
+    const requestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=metric&appid=${apiKey}`
 
     fetch(requestURL)
         .then(function (response) {
@@ -99,13 +97,12 @@ function getForecast(data) {
             } else {
                 response.json()
                     .then(function (data) {
-                        console.log("API One call below")
+                        console.log("DATA from API One call below")
                         console.log(data);
                         renderWeather(data);
-
+                        renderForecast(data);
                     })
             }
-
         }).catch(function (error) {
             console.log(error);
             return error;
@@ -113,45 +110,53 @@ function getForecast(data) {
 
 }
 
+//HERE
 function renderForecast(data) {
+    console.log("data: ", data);
+    forecastEl.innerHTML = "";
 
-    // let forecast = data.daily[i];
-
-    for (var i = 0; i <= 5; i++) {
+    for (var i = 0; i <= 4; i++) {
 
         // console.log(forecast);
 
         let dailyObj = {
             date: data.daily[i].dt,
-            icon: data.daily[i].weather.icon,
-            temp: data.daily[i].temp,
+            icon: data.daily[i].weather[0].icon,
+            temp: data.daily[i].temp.day,
             humidity: data.daily[i].humidity
         }
-        console.log("daily object below");
+        console.log("daily object " + [i] + " below");
         console.log(dailyObj);
 
         let dynamicForecastEl = document.createElement("div");
+        dynamicForecastEl.innerHTML = ""
 
         let dailyDate = document.createElement("h4");
-        dailyDate.textContent = moment.utc(date).format("dddd, Do MMMM");
+        let myDate = moment.unix(dailyObj.date).format('dddd, MMMM Do')
+
+        dailyDate.innerHTML = myDate;
+        console.log("myDate: ", myDate);
         dynamicForecastEl.appendChild(dailyDate);
+
 
         let dailyIcon = document.createElement("img");
         dailyIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + dailyObj.icon + "@2x.png");
         dynamicForecastEl.appendChild(dailyIcon);
 
-        let dailyTemp = document.createElement("p");
-        dailyTemp.textContent = dailyObj.temp;
+        let dailyTemp = document.createElement("h4");
+        dailyTemp.innerHTML = dailyObj.temp + "&#8451;";
         dynamicForecastEl.appendChild(dailyTemp);
 
         let dailyHumidity = document.createElement("p");
-        dailyHumidity.textContent = dailyObj.humidity;
+        dailyHumidity.innerHTML = "Humidity: " + dailyObj.humidity + "%";
         dynamicForecastEl.appendChild(dailyHumidity);
+        dynamicForecastEl.setAttribute("class", "forecast-grid-item");
+
+        forecastEl.appendChild(dynamicForecastEl);
     }
 };
 
 searchBtn.addEventListener('click', handleSearch, renderHistory);
-
 
 function addHistoryButton(city) {
     let historyBtn = document.getElementById("historyBtn");
@@ -162,10 +167,24 @@ function addHistoryButton(city) {
 
 
 function renderHistory() {
-    var prevSearchList = JSON.parse(localStorage.getItem("City"));
     prevSearches.map(function (e) {
         addHistoryButton(e)
     })
 }
 
+
 renderHistory();
+
+
+// TO DO: create function to call search from click on history button
+// on click of history button
+
+// function handleSearchFromHistory(e, city) {
+//     e.preventDefault();
+//     console.log(e.target);
+//     city = e.target.textContent.toLowerCase();
+//     console.log(city);
+//     getLatLon();
+// }
+
+// historyBtn.addEventListener("click", handleSearchFromHistory);
